a775fa1f32cfe39be60f5c61397428e8fe901515
lizhuoran <zhuoran.li.2011@smu.edu.sg>
2017-05-30 03:13:53 +0800

    Initial Commit
    Initial commit

---
+++ src/main/java/com/zhuoran/girtech/farewell/GirtechFarewellApplication.java
@@ -0,0 +1,12 @@
+package com.zhuoran.girtech.farewell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GirtechFarewellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GirtechFarewellApplication.class, args);
+	}
+}
---
+++ src/test/java/com/zhuoran/girtech/farewell/GirtechFarewellApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zhuoran.girtech.farewell;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GirtechFarewellApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
